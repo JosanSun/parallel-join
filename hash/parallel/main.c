20a62f94e84265985b6e6686782907c3d711ea68
spacewander <spacewanderlzx@gmail.com>
2015-06-11 18:50:27 +0800

    make it

---
+++ hash/parallel/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("start parallel join:\n");
+    return 0;
+}
---
+++ hash/serial/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("start serial join:\n");
+    return 0;
+}
+
